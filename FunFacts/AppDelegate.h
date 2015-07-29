be2228e5a40fd096881676b64d287a920568548a
Alex Valladares <alexvalladares@Alexs-Mac.local>
2015-07-29 13:16:42 +0200

    Initial Commit

---
+++ FunFacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunFacts
+//
+//  Created by Alex Valladares on 29/07/15.
+//  Copyright (c) 2015 Alex Valladares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Alex Valladares on 29/07/15.
+//  Copyright (c) 2015 Alex Valladares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
